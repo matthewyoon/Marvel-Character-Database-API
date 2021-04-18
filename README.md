810c4983a02e8eab85eecd953d3f0137b923f051
Matt Yoon <yoon.mattyoon@gmail.com>
2021-04-18 17:38:06 -0400

    heroku-deployment-commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Marvel API Project
+
+## Deployment
+- Deployed on Heroku
+
+# Running on local machine
+- To get all dependencies installed please follow the below steps:
+    - run `pip install -r requirements.txt`
--- config.py
+++ config.py
@@ -10,5 +10,5 @@ class Config():
     """
 
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'You will never guess...'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEPLOY_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
